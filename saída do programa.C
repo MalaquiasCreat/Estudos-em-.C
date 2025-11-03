e7eab687cb2e839c642e3e38a067d29c4c6757ba
Higor.Malaquias <higormalaquias@outlook.com>
2025-11-03 15:14:16 -0300

    feat: exercise to understand the output

---
+++ saída do programa.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    //declara-atribui.valor a variavel a.
+    int a = 5;
+    //declara-atribui.valor a varial b.
+    int b = 3;
+    //exibi o valor da variavel a+b.
+    printf("%d\n", a + b);
+    return 0;
+       
+}
