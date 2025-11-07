a3507606ffd13be6d5cd7d3a2528928dcd13fd18
Higor.Malaquias <higormalaquias@outlook.com>
2025-11-07 16:26:58 -0300

    feat: add menu

--- calculadora.c
+++ calculadora.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+
     float soma(int num1, int num2)
     {
         return num1 + num2;
@@ -30,11 +31,62 @@ int main()
         }
     }
 
-    float res = 0;
+    int numerador = 0;
+    int denomi = 0;
+    int opcao = 0;
+    float resultado = 0.0;
 
-    res = divide(8, 1);
+    do
+    {
+        printf("***escolha uma operacao***\n");
+        printf("1.adicao: \n");
+        printf("2.subtracao:\n");
+        printf("3.multiplicacao: \n");
+        printf("4.divisao: \n");
+        printf("5.Finalizar: \n");
+        scanf("%d", &opcao);
 
-    printf("o resultado e: %.2f", res);
+        if (opcao >= 1 && opcao <= 4)
+        {
+            printf("digite o primeiro numero: \n");
+            scanf("%d", &numerador);
+
+            printf("digite o segundo numero: \n");
+            scanf("%d", &denomi);
+        }
+
+        switch (opcao)
+        {
+        case 1:
+            resultado = soma(numerador, denomi);
+            printf("o resultado e: %.2f\n", resultado);
+            break;
+
+        case 2:
+            resultado = subtrai(numerador, denomi);
+            printf("o resultado e: %.2f\n", resultado);
+            break;
+
+        case 3:
+            resultado = multiplica(numerador, denomi);
+            printf("o resultado e: %.2f\n", resultado);
+            break;
+
+        case 4:
+            resultado = divide(numerador, denomi);
+            printf("o resultado e: %.2f\n", resultado);
+            break;
+
+        case 5:
+            printf("finalizando...\n");
+            break;
+
+        default:
+            printf("opacao invalida... \n");
+        }
+    } while (opcao != 5);
+
+    printf("operacao encerrada\n");
 
     return 0;
 }
